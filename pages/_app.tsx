079555098fca4555c0ce5ce3b683ccb81bbd8db5
Miguel Sierra <miguel-sg01@hotmail.com>
2022-03-05 20:02:18 -0500

    initial project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import Image from 'next/image';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <div className='footer'>
+        <Image
+          alt='Pokemon'
+          src={'/assets/images/pokemonLogo.png'}
+          width={100}
+          height={50}
+        />
+        <p style={{ fontWeight: 200 }}>Developed by Dylas</p>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
